45341e8ab08c6ce39cffb7a49916909aca61ff2a
Sergio <Leal>
2020-12-09 00:58:06 -0600

    Makefile Ram

---
+++ ram/memoria.c
@@ -0,0 +1,7 @@
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/proc_fs.h>
+#include <linux/fs.h>
+#include <linux/sysinfo.h>
+#include <linux/seq_file.h>
+#include <linux/swap.h>
